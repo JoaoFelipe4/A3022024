ba9c98d3c465193a097acc2bd45d7babca9088b3
JoaoFelipe4 <johaoflp93@gmail.com>
2024-10-25 15:05:39 -0300

    add aws library

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
+const AWS = require('aws-sdk')
+const multer = require('multer')
+const upload = muletr({dest: 'uploads/'})
 const app = express()
 const port = 3000
+
+const s3 = new AWS.S3();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/register' , (req,res) => {
+app.post('/register', upload.single('image'), (req,res) => {
+    s3.putObject(req.file)
     res.send('success!')
 })
 
